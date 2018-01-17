07ee28aa80c5331743db8db80fe75a2b30ed1454
Cameron Smith <camerosmit@yahoo.com>
2018-01-17 13:28:22 -0800

    Update the splash screen to wait for user input then load main menu

---
+++ Survive the Pit/Assets/GameAssets/Scripts/SplashManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashManager : MonoBehaviour {
+
+	bool isReady = false;
+	public Animator inputPromptAnim;
+
+	IEnumerator Start()
+	{
+		yield return new WaitForSeconds(2); // TODO Add functionality
+		isReady = true;
+	}
+
+	private void Update()
+	{
+		if (isReady)
+		{
+			inputPromptAnim.SetBool("isReady", true);
+			if (Input.anyKeyDown)
+			{
+				SceneManager.LoadScene(1); // TODO talk to the Scene Manager gameobject
+			}
+		}
+	}
+}

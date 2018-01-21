f62ffec172bc7152025d811dd0f597cd648a5199
Cameron Smith <camerosmit@yahoo.com>
2018-01-21 14:00:51 -0800

    Added a basic MainMenu. Added volume control functionality.

---
+++ Survive the Pit/Assets/GameAssets/Scripts/MenuManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MenuManager : MonoBehaviour {
+
+	public void SetSelectedButton(GameObject go)
+	{
+		EventSystem ES = EventSystem.current;
+		ES.SetSelectedGameObject(go, new BaseEventData(ES));
+	}
+
+	public void Continue()
+	{
+		// TODO Add rich loading functionality
+		Debug.Log("Loading game...");
+	}
+
+	public void NewGame()
+	{
+		// TODO Add rich new game functionality
+		Debug.Log("Creating new game...");
+	}
+
+	public void Quit()
+	{
+		Debug.Log("Quitting...");
+		Application.Quit();
+	}
+}
---
+++ Survive the Pit/Assets/GameAssets/Scripts/SettingsManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class SettingsManager : MonoBehaviour {
+
+	public AudioMixer mixer;
+
+	public void SetMasterVolume(float volume)
+	{
+		mixer.SetFloat("MasterVolume", volume);
+	}
+
+	public void SetBGMVolume(float volume)
+	{
+		mixer.SetFloat("BGMVolume", volume);
+	}
+}

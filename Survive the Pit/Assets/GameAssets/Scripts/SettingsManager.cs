737619a1ea17e63740a11f843bb094b5b4dccdc2
Cameron Smith <camerosmit@yahoo.com>
2018-01-21 22:31:05 -0800

    Updated the menu and Graphics menu. Added the initial build.

--- Survive the Pit/Assets/GameAssets/Scripts/SettingsManager.cs
+++ Survive the Pit/Assets/GameAssets/Scripts/SettingsManager.cs
@@ -3,10 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Audio;
+using TMPro;
 
 public class SettingsManager : MonoBehaviour {
 
 	public AudioMixer mixer;
+
+	public TMP_Dropdown resDropdown;
+	private Resolution[] resolutions;
+
+	private void Start()
+	{
+		resolutions = Screen.resolutions;
+
+		resDropdown.ClearOptions();
+
+		List<string> options = new List<string>();
+
+		int currentResolutionIndex = 0;
+
+		for(int i = 0; i < resolutions.Length; i++)
+		{
+			string option = resolutions[i].width + " x " + resolutions[i].height;
+			options.Add(option);
+
+			if(resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+			{
+				currentResolutionIndex = i;
+			}
+		}
+
+		resDropdown.AddOptions(options);
+		resDropdown.value = currentResolutionIndex;
+		resDropdown.RefreshShownValue();
+	}
 
 	public void SetMasterVolume(float volume)
 	{
@@ -16,5 +46,21 @@ public class SettingsManager : MonoBehaviour {
 	public void SetBGMVolume(float volume)
 	{
 		mixer.SetFloat("BGMVolume", volume);
+	}
+
+	public void SetQuality(int quality)
+	{
+		QualitySettings.SetQualityLevel(quality);
+	}
+
+	public void SetFullscreen(bool isFullscreen)
+	{
+		Screen.fullScreen = isFullscreen;
+	}
+
+	public void SetResolution(int resIndex)
+	{
+		Resolution resolution = resolutions[resIndex];
+		Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
 	}
 }

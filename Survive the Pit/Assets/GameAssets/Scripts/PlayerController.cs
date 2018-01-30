3dc9d6b66d1ec70ea123be48a3a18cea14f6758f
Cameron Smith <camerosmit@yahoo.com>
2018-01-29 21:35:19 -0800

    Added a basic player controller.

---
+++ Survive the Pit/Assets/GameAssets/Scripts/PlayerController.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour {
+
+	// TODO Migrate from Unity Controller to custom controller
+}

f4bd9f4dd6df550f8ba3a23ad236531ff4f6940d
Cameron Smith <camerosmit@yahoo.com>
2018-01-30 09:12:44 -0800

    Split the mouselook portion of PlayerController into two inputs: keyboard and mouse; controller.

--- Survive the Pit/Assets/GameAssets/Scripts/PlayerController.cs
+++ Survive the Pit/Assets/GameAssets/Scripts/PlayerController.cs
@@ -8,8 +8,11 @@ public class PlayerController : MonoBehaviour {
 	Vector2 mouselook;
 	Vector2 smoothV;
 
-	public float sensitivity = 5f;
+	public float mouseSensitivity = 5f;
+	public float controllerSensitivity = 2.5f;
 	public float smoothing = 1f;
+	public bool invertMouse = false;
+	public bool invertController = false;
 
 	public float viewClampUp = -90f;
 	public float viewClampDown = 80f;
@@ -31,6 +34,40 @@ public class PlayerController : MonoBehaviour {
 	private void RotateCamera()
 	{
 		// TODO Split into controller and keyboard sections based on input
+		
+
+		if(InputManager.instance.GetInputState() == InputManager.eInputState.MouseKeyboard)
+		{
+			CreateMouselookV(mouseSensitivity);
+
+			if (invertMouse)
+			{
+				mainCam.transform.localRotation = Quaternion.AngleAxis(-mouselook.y, Vector3.right);
+			}
+			else
+			{
+				mainCam.transform.localRotation = Quaternion.AngleAxis(mouselook.y, Vector3.right);
+			}
+		}
+		else if(InputManager.instance.GetInputState() == InputManager.eInputState.Controller)
+		{
+			CreateMouselookV(controllerSensitivity);
+
+			if (invertController)
+			{
+				mainCam.transform.localRotation = Quaternion.AngleAxis(-mouselook.y, Vector3.right);
+			}
+			else
+			{
+				mainCam.transform.localRotation = Quaternion.AngleAxis(mouselook.y, Vector3.right);
+			}
+		}
+		
+		player.transform.localRotation = Quaternion.AngleAxis(mouselook.x, player.transform.up);
+	}
+
+	private void CreateMouselookV(float sensitivity)
+	{
 		Vector2 md = new Vector2(hInput.GetAxis("RotateCameraX"), hInput.GetAxis("RotateCameraY"));
 
 		md = Vector2.Scale(md, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
@@ -38,8 +75,5 @@ public class PlayerController : MonoBehaviour {
 		smoothV.y = Mathf.Lerp(smoothV.y, md.y, 1f / smoothing);
 		mouselook += smoothV;
 		mouselook.y = Mathf.Clamp(mouselook.y, viewClampUp, viewClampDown);
-
-		mainCam.transform.localRotation = Quaternion.AngleAxis(mouselook.y, Vector3.right);
-		player.transform.localRotation = Quaternion.AngleAxis(mouselook.x, player.transform.up);
 	}
 }
